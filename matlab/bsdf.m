b33cd6dd4b51771d810df5599355f236b623070c
Almar Klein <almar.klein@gmail.com>
2017-10-02 00:32:41 +0200

    improve matlab implementatoon

--- matlab/bsdf.m
+++ matlab/bsdf.m
@@ -12,16 +12,18 @@ function result = bsdf(arg1, arg2)
 %
 % This file is freely distributed under the terms of the 2-clause BSD License.
 % Copyright (C) 2017 Almar Klein
-        
+    
+    VERSION = [2, 0, 0];
+    
     if nargin == 1
         % filename, bytes, or object
         if isa(arg1, 'char') && sum(arg1 == sprintf('\n')) == 0
             % Load from file, exists?
             if ~exist(arg1, 'file');  error([mfilename ': the specified file does not exist.']);  end;            
             % Read file   
-            f = fopen(arg1, 'r', 'l');  % read / little endian
+            f = fopen(arg1, 'r', 'l');%, 'UTF-8');  % read / little endian
             try
-                data = load(f);
+                data = load(f, VERSION);
                 fclose(f);
             catch e
                 fclose(f);
@@ -31,13 +33,13 @@ function result = bsdf(arg1, arg2)
         
         elseif isa(arg1, 'uint8') && isequal(size(arg1), [1, numel(arg1)])
             % Load from bytes
-            f = fopen(Tempfile, 'w', 'l');  % write / little endian
+            f = fopen(Tempfile, 'w', 'l');%, 'UTF-8');  % write / little endian
             tempfilename = fname(f);
             fwrite(f, arg1);
             fclose(f);
-            f = fopen(tempfilename, 'r', 'l');
+            f = fopen(tempfilename, 'r', 'l');%, 'UTF-8');
             try
-                data = load(bytes);
+                data = load(bytes, VERSION);
                 fclose(f);
             catch e
                 fclose(f);
@@ -48,31 +50,31 @@ function result = bsdf(arg1, arg2)
         
         else
             # Serialize to bytes
-            f = fopen(Tempfile, 'w', 'l');  % write / little endian
+            f = fopen(Tempfile, 'w', 'l');%, 'UTF-8');  % write / little endian
             tempfilename = fname(f);
             try
-                save(f, arg1);
+                save(f, arg1, VERSION);
                 fclose(f);
             catch e
                 fclose(f);
                 rethrow(e);
             end
-            f = fopen(tempfilename, 'r', 'l');
+            f = fopen(tempfilename, 'r', 'l');%, 'UTF-8');
             bytes = fread(f, inf);
             fclose(f);
             delete(tempfilename);
             result = bytes;
         end
         
-    elseif nargin==2
-        % Write to file, exists?
-        if ~isa(arg1, 'char') || ~exist(arg1, 'file')
+    elseif nargin == 2
+        % Write to file, looks like file? Does not have to exist yet, of course
+        if ~isa(arg1, 'char') 
              error([mfilename ': Invalid filename given.']); 
         end        
         % Write to file
-        f = fopen(arg1, 'w', 'l');  % write / little endian
+        f = fopen(arg1, 'w', 'l');%, 'UTF-8');  % write / little endian
         try
-            save(f, arg1);
+            save(f, arg2, VERSION);
             fclose(f);
         catch e
             fclose(f);
@@ -87,37 +89,49 @@ function result = bsdf(arg1, arg2)
 end
 
 
-VERSION = [2, 0, 0];
-
-function save(f, ob)
-    % Write header
-    fwrite(f, [66; 83; 68; 70]);
-    % Write version
-    fwrite(f, VERSION(0));
-    fwrite(f, VERSION(1));
-    % Go!
-    encode(f, ob);
+function write_length(f, x)
+    % Encode an unsigned integer into a variable sized blob of bytes.
+    if x <= 250
+        fwrite(f, x, 'uint8');
+    # elif x < 65536:
+    #     return spack('<BH', 251, x)
+    # elif x < 4294967296:
+    #     return spack('<BI', 252, x)
+    else
+        fwrite(f, x, 'uint64');
+    end
 end
 
 
-function ob = load(f)
+function save(f, ob, VERSION)
+    % Write header
+    fwrite(f, [66; 83; 68; 70]);
+    % Write version
+    fwrite(f, VERSION(1));
+    fwrite(f, VERSION(2));
+    % Go!
+    bsdf_encode(f, ob);
+end
+
+
+function ob = load(f, VERSION)
     % Get header
     head = fread(f, 4, '*uint8');
     assert(isequal(head, [66; 83; 68; 70]), 'Not a valid BSDF file');    
     % Process version 
     major_version = fread(f, 1, '*uint8');
     minor_version = fread(f, 1, '*uint8');
-    if major_version != VERSION[1]
+    if major_version != VERSION(1)
         error([mfilename ': file major version does not match implementation version.']);
-    elseif minor_version > VERSION[1]
+    elseif minor_version > VERSION(2)
         warning([mfilename ': file minor version is higher than implementation.'])
     end
     % Go!
-    ob = decode(f);
+    ob = bsdf_decode(f);
 end
 
 
-function encode(f, value)
+function bsdf_encode(f, value)
   
     % todo: convert ...
     
@@ -130,17 +144,19 @@ function encode(f, value)
             for i=1:length(keys)
                 key = keys{i};
                 val = value.(key);
-                write_length(f, length(key));
-                encode(f, val);
+                key_b = uint8(key);  % this assumes ASCII keys - UTF-8 is not trivial in Matlab/Octave
+                write_length(f, length(key_b));
+                fwrite(f, key_b);
+                bsdf_encode(f, val);
             end
         
         case 'cell'
             fwrite(f, 'l');            
             write_length(f, length(value));
             for i=1:length(value)
-                encode(f, value{i});
+                bsdf_encode(f, value{i});
             end
-          
+        
         case 'char'
             fwrite(f, 's');            
             write_length(f, length(value));
@@ -196,52 +212,53 @@ function encode(f, value)
 end
 
 
-function value = decode(f)
-    char = fread(f, 1, 'char');
-    c = lower(char);
+function value = bsdf_decode(f)
+    the_char = fread(f, 1, 'char');
+    c = lower(the_char);
     
-    if numel(char) == 0
+    if numel(the_char) == 0
         error('bsdf:eof', 'end of file');
-    elseif ~isequal(c, char)
+    elseif ~isequal(c, the_char)
         error('cannot do custom data yet');
+    end
     
     if c == 'v'
         value = [];  % null
     elseif c == 'y'
-        value = true;
+        value = 1;
     elseif c == 'n'
-        value = false;
+        value = 0;
     elseif c == 'u'
-        fread(f, 1, 'uint8');  % int as uint8
+        value = fread(f, 1, 'uint8');  % int as uint8
     elseif c == 'i'
-        fread(f, 1, 'int64');  % int as int64
+        value = fread(f, 1, 'int64');  % int as int64
     elseif c == 'f'
-        fread(f, 1, 'float32');  % float32
+        value = fread(f, 1, 'float32');  % float32
     elseif c == 'd'
-        fread(f, 1, 'float64');  % float64
+        value = fread(f, 1, 'float64');  % float64
     elseif c == 's'        
         n = fread(f, 1, '*uint8');
         if n == 253; n = fread(f, 1, 'uint64'); end;
-        value = fread(f, n, 'char');
+        value = char(fread(f, n, 'uint8')');
     elseif c == 'l'
-        n = fread(f, 1, '*uint8');
-        stream = 0;
+        n = fread(f, 1, '*uint8');        
+        stream = 0;        
         if n == 253
             n = fread(f, 1, 'uint64');
         elseif n == 255
              n = fread(f, 1, 'uint64');  % zero if not closed
              stream = 1;
         end
-        if stream && n == 0
+        if stream == 1 && n == 0
             % Unclosed stream, keep reading until fail
             value = {};
             try
                 while 1
-                    value{end+1} = decode(f);
+                    value{end+1} = bsdf_decode(f);
                 end
             catch e
                 if ~isequal(e.identifier, 'bsdf:eof')
-                  rethrow(e);
+                    rethrow(e);
                 end
             end
         else
@@ -249,7 +266,7 @@ function value = decode(f)
             value = {};
             value{n} = 0;
             for i=1:n
-                value{i} = decode(f);
+                value{i} = bsdf_decode(f);
             end
         end
     elseif c == 'm'
@@ -259,9 +276,8 @@ function value = decode(f)
         for i=1:n
             n_name = fread(f, 1, '*uint8');
             if n_name == 253; n_name = fread(f, 1, 'uint64'); end;
-            name = fread(f, n, 'char');
-            child = children{i};
-            value.(name) = decode(f);            
+            name = char(fread(f, n_name, 'uint8')');
+            value.(name) = bsdf_decode(f);
         end       
     elseif c == 'b'
         % Blob of bytes - header is 5 to 42 bytes
@@ -301,25 +317,8 @@ function value = decode(f)
         % Skip extra space
         fread(f, allocated_size - used_size, '*uint8');
     else
-        error([mfilename ': parse error']);
+        error([mfilename ': unknown data type ' c]);
     end
     
     % todo: Convert value
-    
-end
-
-
-function write_length(f, x)
-    % Encode an unsigned integer into a variable sized blob of bytes.
-    if x <= 250
-        fwrite(f, x, 'uint8');
-    # elif x < 65536:
-    #     return spack('<BH', 251, x)
-    # elif x < 4294967296:
-    #     return spack('<BI', 252, x)
-    else
-        fwrite(f, x, 'uint64');
-    end
-end
-
 end
--- matlab/service_runner.m
+++ matlab/service_runner.m
@@ -1,5 +1,25 @@
 
+addpath('jsonlab');
+
 fname1 = argv(){1};
 fname2 = argv(){2};
 
-data = bsdf(fname1);
+if strfind(fname1, '.json')  % endsWith not available in Octave
+    data = loadjson(fname1, 'SimplifyCell', 0, 'FastArrayParser', 0);
+elseif strfind(fname1, '.bsdf')
+    data = bsdf(fname1);
+else
+    error('Expected a json or bsdf file.')
+end
+
+'euh wat'
+data
+
+if strfind(fname2, '.json')
+    savejson('', data, fname2);
+elseif strfind(fname2, '.bsdf')
+    bsdf(fname2, data);
+else
+    error('Expected a json or bsdf file.')
+end
+  
