89ad83d4fb00834f124f77f122222d9452c20bcb
Almar Klein <almar.klein@gmail.com>
2017-10-12 16:37:54 +0200

    node or nodejs

--- javascript/tasks.py
+++ javascript/tasks.py
@@ -12,15 +12,27 @@ def call(*cmd):
     sys.exit(subprocess.call(cmd, cwd=this_dir))
 
 
+def get_node_exe():
+    node_exe = 'nodejs'
+    try:
+        subprocess.check_output([node_exe, '--version'])
+    except Exception:
+        node_exe = 'node'
+    return node_exe
+
+
 @task
 def test_shared(ctx):
     """ Run BSDF tests on JS using the shared test service. """
     sys.path.insert(0, os.path.join(this_dir, '..', '_tools'))
     import testservice
-    testservice.main(this_dir, 'node', 'testservice_runner.js', '{fname1}', '{fname2}',
+    exe = get_node_exe()
+    testservice.main(this_dir, exe, 'testservice_runner.js', '{fname1}', '{fname2}',
                      excludes=['roundfloatisnotint'])
+
 
 @task
 def test_unit(ctx):
     """ Run unit tests for JS BSDF implementation. """
-    call('node', 'test_unit.js')
+    exe = get_node_exe()
+    call(exe, 'test_unit.js')
